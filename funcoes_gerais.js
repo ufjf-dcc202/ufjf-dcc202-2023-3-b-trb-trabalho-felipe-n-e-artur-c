308bb0163b8b6ff250ea6e71f3774f8c180bc444
Artur Ferreira de Castro <artur.castro@estudante.ufjf.br>
2023-12-11 00:54:07 -0300

    Adiciona função clicarColuna

--- funcoes_gerais.js
+++ funcoes_gerais.js
@@ -6,3 +6,39 @@ function trocaClasse(d){
     tabuleiro.classList.add(dados[prox-1]);
 
 }
+
+//Função que ativa ao clicar na célula
+const clicarColuna = (x) => {
+    // Botar o dado
+    const cell = x.target;
+    let dadoSerAdicionado = prox;
+    addDado(cell,dadoSerAdicionado);
+    
+    //Começo da ideia de como deletar dados
+    //deletaDado(celulas2,indexApagar,valorCell2);
+    
+    //OBS: descobrir como saber o index da célula clicada
+    
+    
+    dadoCanto.innerHTML = dadoCanto.classList.remove(dados[prox-1]);
+    
+    //Verifica presença de dado e calcula valor de cada coluna
+    verifValor(x);
+
+    getProx();
+    dadoCanto.innerHTML = dadoCanto.classList.add(dados[prox-1]); 
+
+    //Troca classe
+    trocaClasse(dadoSerAdicionado);
+
+    //Bot joga
+    botJoga();
+
+
+    // Fim de Jogo
+    const acabou = endGame("d1","d2","d3","d4","d5","d6");
+    if (acabou === true){
+        console.log('acabou rs');
+        
+    }
+}
